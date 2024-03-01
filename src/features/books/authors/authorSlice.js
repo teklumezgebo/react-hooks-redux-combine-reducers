a205300770709893d8bcb4722bdf549654e59b02
Teklu Mezgebo <mezge002@umn.edu>
2024-02-29 18:15:39 -0600

    two reducers created, reducers combined

---
+++ src/features/books/authors/authorSlice.js
@@ -0,0 +1,28 @@
+export const addAuthor = (author) => {
+    return {
+        type: "authors/add",
+        payload: author
+    }
+}
+
+export const removeAuthor = (id) => {
+    return {
+        type: "authors/remove",
+        payload: id
+    }
+}
+
+const initialState = []
+
+export default function authorsReducer(state = initialState, action) {
+    switch (action.type) {
+        case "authors/add":
+            return [...state, action.payload]
+        
+        case "authors/remove":
+            return state.filter((author) => author.id !== action.payload)
+        
+        default:
+            return state
+    }
+}
---
+++ src/features/books/books/booksSlice.js
@@ -0,0 +1,28 @@
+export const addBook = (book) => {
+  return {
+    type: "books/add",
+    payload: book,
+  };
+};
+
+export const removeBook = (id) => {
+  return {
+    type: "books/remove",
+    payload: id,
+  };
+};
+
+const initialState = []
+
+export default function booksReducer(state = initialState, action) {
+  switch (action.type) {
+    case "books/add":
+      return [...state, action.payload]
+
+    case "books/remove":
+      return state.filter((book) => book.id !== action.payload)
+    
+    default:
+      return state
+  }
+}
